DETR normalization layer fix

maps_vectorization/models_src/DETR.py
@@ -55,16 +55,15 @@ class LearnablePositionalEncoding(tf.keras.layers.Layer):
         self.rows_embed = self.add_weight(shape=(self.H,1,self.D//2), initializer=self.initializer, trainable=True)
         self.cols_embed = self.add_weight(shape=(1,self.W,self.D//2), initializer=self.initializer, trainable=True)
 
-    def call(self, inputs):
-
         pos = tf.concat([
             tf.repeat(self.cols_embed, self.H, axis=0),
             tf.repeat(self.rows_embed, self.W, axis=1)
         ], axis=-1)
 
-        pos = tf.expand_dims(tf.reshape(pos, (self.H*self.W, self.D)), axis=0)
+        self.pos = tf.reshape(pos, (self.H*self.W, self.D))
 
-        return pos
+    def call(self, inputs):
+        return self.pos
     
 class FlatLearnablePositionalEncoding(tf.keras.layers.Layer):
     def __init__(self, num_queries, emb_dim, initializer=tf.keras.initializers.GlorotUniform(), **kwargs):
@@ -150,6 +149,15 @@ class MHA(tf.keras.layers.Layer):
         return self.O_d(self.output_perm(tf.matmul(weights, V)))
 
 
+class DeepLayerNormalization(tf.keras.layers.Layer):
+    def __init__(self, norm_axis=1, **kwargs):
+        super(DeepLayerNormalization, self).__init__(**kwargs)
+        self.axis = norm_axis
+
+    def call(self, inputs, training=None):
+        norm_mean = tf.reduce_mean(inputs, axis=self.axis, keepdims=True)
+        norm_std = tf.math.reduce_std(inputs, axis=self.axis, keepdims=True)
+        return tf.math.divide_no_nan((inputs-norm_mean),norm_std)
 
 class EncoderLayer(tf.keras.layers.Layer):
     def __init__(self, 
@@ -166,7 +174,7 @@ class EncoderLayer(tf.keras.layers.Layer):
         self.attn_dropout, self.output_dropout = [tf.keras.layers.Dropout(dropout) for _ in range(2)]
         self.attn_addnorm, self.output_addnorm = [tf.keras.Sequential([
             tf.keras.layers.Add(),
-            tf.keras.layers.LayerNormalization()])
+            DeepLayerNormalization(norm_axis=1)])
             for _ in range(2)]
 
         self.FFN = FFN(FFN_mid_layers, FFN_mid_units, attn_dim, dropout, FFN_activation)
@@ -199,7 +207,7 @@ class DecoderLayer(tf.keras.layers.Layer):
         self.QO_dropout, self.MEM_dropout, self.output_dropout = [tf.keras.layers.Dropout(dropout) for _ in range(3)]
         self.QO_addnorm, self.MEM_addnorm, self.output_addnorm = [tf.keras.Sequential([
             tf.keras.layers.Add(),
-            tf.keras.layers.LayerNormalization()])
+            DeepLayerNormalization(norm_axis=1)])
             for _ in range(3)]
 
         self.FFN = FFN(FFN_mid_layers, FFN_mid_units, attn_dim, dropout, FFN_activation)
@@ -237,7 +245,13 @@ class DETRUpConv(tf.keras.layers.Layer):
         x = self.dropout(x, training=training)
 
         return x
-    
+
+class ZeroLikeLayer(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(ZeroLikeLayer, self).__init__(**kwargs)
+
+    def call(self, inputs, training=None):
+        return tf.zeros_like(inputs)
 
 class DETRTransformer(CombinedMetricsModel):
     def __init__(self, 
@@ -260,77 +274,74 @@ class DETRTransformer(CombinedMetricsModel):
                  mask_output=False,
                  metrics=[],
                  **kwargs):
-        super(DETRTransformer, self).__init__(**kwargs)
 
         backbone = tf.keras.applications.vgg16.VGG16(include_top=False, input_shape=input_shape)
-        self.backbone = tf.keras.Model(backbone.inputs, backbone.layers[-2].output, name='Backbone')
-        backbone_shape = self.backbone.output_shape
+        backbone = tf.keras.Model(backbone.inputs, backbone.layers[-2].output, name='Backbone')
+        backbone_shape = backbone.output_shape
 
-        self.feature_extraction = tf.keras.layers.Conv2D(attn_dim, 1, name='Features-Extraction')
+        feature_extraction = tf.keras.layers.Conv2D(attn_dim, 1, name='Features-Extraction')
 
-        self.image_flatten = tf.keras.layers.Reshape((-1, attn_dim), name='Image-Flattening')
+        image_flatten = tf.keras.layers.Reshape((-1, attn_dim), name='Image-Flattening')
 
-        self.pos_enc_func = pos_encoder
-        self.QO_enc_func = FlatLearnablePositionalEncoding(queries_num, key_dim, name='QueryObjects-PosEncoder')
+        pos_enc_func = pos_encoder
+        QO_enc_func = FlatLearnablePositionalEncoding(queries_num, key_dim, name='QueryObjects-PosEncoder')
 
-        self.encoder_layers = [EncoderLayer(attn_dim, key_dim, num_heads, dropout, 
+        encoder_layers = [EncoderLayer(attn_dim, key_dim, num_heads, dropout, 
                                             FFN_mid_layers, FFN_mid_units, FFN_activation,
                                             name=f'Encoder-{n}') for n in range(layers_num)]
         
-        self.decoder_layers = [DecoderLayer(attn_dim, key_dim, num_heads, dropout, 
+        decoder_layers = [DecoderLayer(attn_dim, key_dim, num_heads, dropout, 
                                             FFN_mid_layers, FFN_mid_units, FFN_activation,
                                             name=f'Decoder-{n}') for n in range(layers_num)]
-        self.mask_output = mask_output
+        mask_output = mask_output
         if mask_output:
-            self.img_unsqueeze = tf.keras.layers.Reshape((backbone_shape[1], backbone_shape[2], attn_dim), name='IMG-Unsqueeze')
-            self.upconvs = [DETRUpConv(filters=attn_dim, name=f'UpConv-{n}') for n in range(upconv_blocks)]
-            self.output_conv = tf.keras.layers.Conv2D(output_dim, 1, activation='sigmoid',name='Output-Conv')
+            img_unsqueeze = tf.keras.layers.Reshape((backbone_shape[1], backbone_shape[2], attn_dim), name='IMG-Unsqueeze')
+            upconvs = [DETRUpConv(filters=attn_dim, name=f'UpConv-{n}') for n in range(upconv_blocks)]
+            output_conv = tf.keras.layers.Conv2D(output_dim, 1, activation='sigmoid',name='Output-Conv')
         else:
-            self.out_FFN = FFN(FFN_out_layers, FFN_out_mid_units, 5, dropout, FFN_out_activation, name='Out-FFN')
-            self.out_sigmoid = tf.keras.layers.Activation('sigmoid', name='Out-Sigmoid')
+            out_FFN = FFN(FFN_out_layers, FFN_out_mid_units, 5, dropout, FFN_out_activation, name='Out-FFN')
+            out_sigmoid = tf.keras.layers.Activation('sigmoid', name='Out-Sigmoid')
+
+        inputs = tf.keras.layers.Input(input_shape)
+        #backbone
+        features = feature_extraction(backbone(inputs))
+        
+        pos_enc = pos_enc_func(features)
+        memory = image_flatten(features)
+
+        #Encoder
+        for encoder_layer in encoder_layers:
+            memory = encoder_layer(memory, pos_enc)
+
+        #Decoder
+        QO_enc = QO_enc_func(features)
+        QO = ZeroLikeLayer(name='QueryObjects')(QO_enc)
+
+        for decoder_layer in decoder_layers:
+            QO = decoder_layer(QO, memory, pos_enc, QO_enc)
+
+        if mask_output:
+            # Upscale output images
+            output = img_unsqueeze(QO)
+
+            for upconv in upconvs:
+                output = upconv(output)
+
+            output = output_conv(output)
+            output = tf.clip_by_value(output, 1e-8, 1-1e-8)
+        else:
+            output = out_sigmoid(out_FFN(QO))
+            confidence, bboxes = self._bbox_decoding(output)
+            confidence = tf.squeeze(confidence, axis=2)
+            output = {'class': confidence, 'bbox': bboxes}
+
+        super(DETRTransformer, self).__init__(inputs=inputs, outputs=output, **kwargs)
 
         self.add_metrics(metrics)
-
-        self.call(tf.keras.layers.Input(input_shape))
+        #self.call(tf.keras.layers.Input(input_shape))
 
     def _bbox_decoding(self, x):
         C, YX, HW = x[...,:1], x[...,1:3], x[...,3:]*0.5
 
         bboxes = tf.concat([tf.clip_by_value(YX-HW, [0,0], [1,1]), tf.clip_by_value(YX+HW, [0,0], [1,1])], axis=-1)
         return C, bboxes
-        
-
-    def call(self, inputs, training=None):
-
-        #backbone
-        features = self.feature_extraction(self.backbone(inputs, training=training))
-        
-        pos_enc = self.pos_enc_func(features)
-        memory = self.image_flatten(features)
-
-        #Encoder
-        for encoder_layer in self.encoder_layers:
-            memory = encoder_layer(memory, pos_enc, training=training)
-
-        #Decoder
-        QO_enc = self.QO_enc_func(None)
-        QO = tf.zeros_like(QO_enc)
-
-        for decoder_layer in self.decoder_layers:
-            QO = decoder_layer(QO, memory, pos_enc, QO_enc, training=training)
-
-        if self.mask_output:
-            # Upscale output images
-            output = self.img_unsqueeze(QO)
-
-            for upconv in self.upconvs:
-                output = upconv(output)
-
-            output = self.output_conv(output)
-            output = tf.clip_by_value(output, 1e-8, 1-1e-8)
-        else:
-            output = self.out_sigmoid(self.out_FFN(QO))
-            confidence, bboxes = self._bbox_decoding(output)
-            confidence = tf.squeeze(confidence, axis=2)
-            return {'class': confidence, 'bbox': bboxes}
-        return output