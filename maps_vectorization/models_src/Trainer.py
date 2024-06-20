f5d6c7cfc185ead6f3eb7911fddad819e94036d4
fzzyfoxx <fzzyfoxx@wp.pl>
2024-06-20 12:58:43 +0200

    mlflow trainer adaptation for fourier models

--- maps_vectorization/models_src/Trainer.py
+++ maps_vectorization/models_src/Trainer.py
@@ -293,3 +293,181 @@ class TrainingProcessor:
             if type(background).__name__!='NoneType':
                 ax.imshow(background)
             ax.imshow(pred, cmap=cmap, alpha=alpha)
+
+
+
+class TrainingProcessor2:
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.cfg_attrs = self._get_cfg_attrs()
+
+    def load_dataset(self, ds, train_steps, val_ds=None, val_steps=None):
+        self.ds = ds
+        self.train_steps = train_steps
+        self.val_ds = val_ds
+        self.val_steps = val_steps
+
+        self.cfg.train_samples = train_steps*cfg.train_batch_size
+        self.cfg.val_samples = val_steps*cfg.val_batch_size
+
+    def _get_cfg_attrs(self):
+        return [(a, getattr(self.cfg, a)) for a in dir(self.cfg) if not a.startswith('__')]
+    
+    def load_model_generator(self, model_generator):
+        self.model_generator = model_generator
+        self.cfg.generator_name = model_generator.__name__
+    
+    def _gen_custom_model(self, model_args):
+        return self.model_generator(**model_args)
+
+    def _log_mlflow_params(self,):
+        
+        for key in self.model_args.keys():
+            mlflow.log_param(key, self.model_args[key])
+
+        for key, value in self.cfg_attrs:
+            mlflow.log_param(key, value)
+
+        for name, loss_args in self.loss_args:
+            for key, value in loss_args.items():
+                mlflow.log_param('loss.'+name+'_'+key, value)
+
+            #mlflow.log_param('model_type', self.model_type)
+
+    def compile_model(self, model_args, optimizer, loss, metrics=None, weighted_metrics=None, loss_weights=None, print_summary=True):
+        self.model_args = model_args
+        #self.model_type = model_type
+        self.loss_args = [(k, v.get_config()) for k, v in  loss.items()]
+
+        self.model = self._gen_custom_model(model_args)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.model.compile(
+                optimizer=optimizer,
+                loss = loss,
+                metrics = metrics,
+                loss_weights = loss_weights,
+                weighted_metrics = weighted_metrics
+                )
+
+        if print_summary:
+            print(self.model.summary(expand_nested=True))
+
+        self.initial_epoch = 0
+
+    def _get_custom_measures(self,loss, metrics):
+        if metrics is None:
+            metrics = {}
+            
+        custom_objects = {}
+        for c in {**loss, **(metrics if metrics.__class__ == dict else dict([(metric.name, metric) for metric in metrics]))}.values():
+            if c.__module__.split('.')[0]!='keras':
+                custom_objects[c.__class__.__name__] = c
+        return custom_objects
+
+    def train_model(self, epochs, log=True, callbacks=None, export_final_state=True, export_model=True, validation_freq=1):
+
+        if log:
+            #mlflow.tensorflow.autolog(log_datasets=False, log_models=True, disable=False)
+            mlflow.start_run()
+            print(f'MLflow run: {mlflow.active_run().info.run_name}')
+        else:
+            None
+            #mlflow.tensorflow.autolog(log_datasets=False, log_models=False, disable=True)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.model.fit(self.ds, 
+                           validation_data=self.val_ds if (validation_freq>0) & (self.val_ds is not None) else None,
+                           validation_steps=self.val_steps if (validation_freq>0) & (self.val_steps is not None) else None,
+                           validation_freq=validation_freq,
+                           epochs=epochs+self.initial_epoch, 
+                           steps_per_epoch=self.train_steps, 
+                           callbacks=callbacks, 
+                           initial_epoch=self.initial_epoch)
+        self.initial_epoch += epochs
+
+        
+
+        if log:
+            self._log_mlflow_params()
+            if export_final_state:
+                #save weights
+                self.model.save_weights('./final_state/variables')
+                mlflow.log_artifacts('./final_state', 'final_state')
+            if export_model:
+                mlflow.tensorflow.log_model(self.model, 'model',  custom_objects=self._get_custom_measures(self.model.loss, self.model.metrics))
+        
+        try:
+            mlflow.end_run()
+        except:
+            None
+
+    ### LOAD MODEL
+
+    def _load_mlflow_args(self, run_args):
+        cfg_params = {}
+        # fit method parameters
+        fit_args = ['params.'+p for p in inspect.getfullargspec(tf.keras.Model.fit)[0]]
+        run_args = run_args[~run_args.index.str.contains('opt_', case=False) & run_args.index.str.contains('params.') & ~run_args.index.isin(fit_args)]
+
+        # get loss attributes
+        loss_args = {key.split('.')[-1]: value for key, value in run_args[run_args.index.str.contains('loss.')].to_dict().items()}
+        run_args = run_args[~run_args.index.str.contains('loss.')]
+
+        # look for general configuration parameters
+        for key, _ in self.cfg_attrs:
+            search_results = [(p,run_args[p]) for p in run_args.index if 'params.'+key==p]
+            if len(search_results)>0:
+                param, value = search_results[0]
+                cfg_params[key] = getattr(__builtins__, type(getattr(self.cfg,key)).__name__)(value)
+                run_args = run_args.drop(param)
+
+        # get model args
+        model_args = {key.split('.')[-1]: value for key, value in run_args.to_dict().items()}
+
+        if not np.all([getattr(self.cfg, key)==value for key, value in cfg_params.items()]):
+            for key, value in cfg_params.items():
+                setattr(self.cfg, key, value)
+
+
+        self.loss_args = loss_args
+        self.model_args = model_args
+
+        print('\n\033[1mLoss params\033[0m')
+        print(loss_args)
+
+        print('\n\033[1mModel params\033[0m')
+        print(model_args)
+
+    def save_temp_weights(self, weights_path):
+        try:
+            os.mkdir(weights_path)
+        except:
+            None
+        self.model.save_weights(f'./{weights_path}/weights.keras', save_format='keras')
+
+    def load_temp_weights(self, weights_path, skip_mismatch=True, by_name=True):
+        self.model.load_weights(f'./{weights_path}/weights.keras', skip_mismatch, by_name)
+
+    def load_mlflow_weights(self, run_name):
+        run_args = mlflow.search_runs(filter_string=f"tags.mlflow.runName like '{run_name}%'").iloc[0]
+        #self.initial_epoch = int(run_args['params.epochs'])
+        mlflow.artifacts.download_artifacts(run_id=run_args.run_id,artifact_path='final_state',
+                                    dst_path='./')
+        self.model.load_weights('final_state/variables')
+
+    def load_model(self, run_name, load_final_state=True):
+        run_args = mlflow.search_runs(filter_string=f"tags.mlflow.runName like '{run_name}%'").iloc[0]
+        self.initial_epoch = int(run_args['params.epochs']) #int(run_args['params.initial_epoch']) + 
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.model = mlflow.tensorflow.load_model(os.path.join(run_args.artifact_uri, "model"))
+
+        if load_final_state:
+            mlflow.artifacts.download_artifacts(run_id=run_args.run_id,artifact_path='final_state',
+                                    dst_path='./')
+            self.model.load_weights('final_state/variables')
+
+        self._load_mlflow_args(run_args)
