08218ff66ea2429777d574545edc772ff8696330
fzzyfoxx <fzzyfoxx@wp.pl>
2024-11-29 11:49:29 +0100

    dict_widget update - dict widget display now can handle nested structures

--- maps_vectorization/exp_lib/utils/dict_widget.py
+++ maps_vectorization/exp_lib/utils/dict_widget.py
@@ -32,33 +32,46 @@ dark_mode_css = """
 </style>
 """
 
-def create_widgets_for_dict(d):
+def set_widget_title(widget, title):
+    widget.layout.display = 'flex'
+    widget.layout.flex_direction = 'column'
+    widget.layout.align_items = 'center'
+    widget.layout.justify_content = 'center'
+    widget.children = [widgets.Label(value=title), widget.children[0]]
+def create_widgets_for_dict(d, css=""):
     widgets_dict = {}
     for key, value in d.items():
         layout = widgets.Layout(width='300px', height='30px')
         style = {'description_width': 'initial'}
-        if isinstance(value, bool):
-            widget = widgets.Checkbox(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text','custom-checkbox'))
-        elif isinstance(value, int):
-            widget = widgets.IntText(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text',))
-        elif isinstance(value, float):
-            widget = widgets.FloatText(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text',))
-        elif isinstance(value, str):
-            widget = widgets.Text(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text',))
+
+        if isinstance(value, dict):
+            display_dict(value, css=css, nested=True, nested_title=key)
         else:
-            continue
-        widgets_dict[key] = widget
+            if isinstance(value, bool):
+                widget = widgets.Checkbox(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text','custom-checkbox'))
+            elif isinstance(value, int):
+                widget = widgets.IntText(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text',))
+            elif isinstance(value, float):
+                widget = widgets.FloatText(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text',))
+            elif isinstance(value, str):
+                widget = widgets.Text(value=value, description=key, layout=layout, style=style, _dom_classes=('custom-int-text',))
+            else:
+                continue
+
+            widgets_dict[key] = widget
     return widgets_dict
 
 def update_dict_from_widgets(d, widgets_dict):
     for key, widget in widgets_dict.items():
         d[key] = widget.value
 
-def display_dict(d, trainer=None, compile_args_func=None, compile_args={}, css=dark_mode_css):
+def display_dict(d, trainer=None, compile_args_func=None, compile_args={}, css=dark_mode_css, nested=False, nested_title=None):
 
+    if nested:
+        css = f'<h5> {nested_title} </h5>'
     display(HTML(css))
 
-    widgets_dict = create_widgets_for_dict(d)
+    widgets_dict = create_widgets_for_dict(d, css=css)
     widget_list = list(widgets_dict.values())
     
     # Create a GridBox to display widgets in 4 columns
@@ -69,7 +82,7 @@ def display_dict(d, trainer=None, compile_args_func=None, compile_args={}, css=d
     
     display(grid)
     
-    update_button = widgets.Button(description="Accept Parameters")
+    update_button = widgets.Button(description="Accept Parameters" if not nested else "Pass Parameters")
     display(update_button)
     
     def on_update_button_clicked(b):
