ae61feb93e072b02c123cf4bb04bb026b13eb1c4
fzzyfoxx <fzzyfoxx@wp.pl>
2025-05-22 10:09:55 +0200

    Strategize self conv research class

--- focus_group/fcgb/src/fcgb/types/self_conv.py
+++ focus_group/fcgb/src/fcgb/types/self_conv.py
@@ -1,5 +1,22 @@
-from typing import List
+from typing import List, TypedDict, Dict
 from pydantic import BaseModel
 
 class SelfConvModel(BaseModel):
     answer: str
+
+class SingleStrategyModel(TypedDict):
+    strategy_description: str
+    paraphrased_task: str
+    paraphrased_context: str
+
+class StrategyTaskModel(BaseModel):
+    strategies: List[SingleStrategyModel]
+
+class Strategyroutingstate(BaseModel):
+    template_inputs: Dict[str, str]
+    sc_thread_id: str
+    parent_thread_id: str
+    sc_summary: str | None
+
+class VerificationPromptsModel(BaseModel):
+    verification_prompts: List[str]
