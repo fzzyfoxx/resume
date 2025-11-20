e45554f8e7533faec7ef06abbe2d2b6f9a3bb933
fzzyfoxx <fzzyfoxx@wp.pl>
2025-11-20 13:06:01 +0100

    geodoc_config sphinx documentation build

--- geodoc_config/geodoc_config/src/loader.py
+++ geodoc_config/geodoc_config/src/loader.py
@@ -9,28 +9,25 @@ def load_json(path):
 def load_config_by_path(folder, file):
     """
     Load a configuration file from a specified folder and file name.
+
     Args:
         folder (str): The folder where the configuration file is located.
         file (str): The name of the configuration file.
+
     Returns:
         dict: The loaded configuration data.
     """
-    # Access the specified configuration file within the geodoc_config package
     config_path = files(f"geodoc_config.configs.{folder}").joinpath(file)
-    
-    # Load and return the JSON data
     return load_json(config_path)
-    
+
 def load_mapping():
     """
     Load the mapping configuration from the package's resources.
+
     Returns:
         dict: The mapping configuration loaded from the JSON file.
     """
-    # Access the configs_map.json file within the geodoc_config package
     config_path = files("geodoc_config.configs.mapping").joinpath("configs_map.json")
-    
-    # Load and return the JSON data
     return load_json(config_path)
 
 def show_config_files():
@@ -43,8 +40,10 @@ def show_config_files():
 def get_config_file_path(config_name):
     """
     Get the path to a specific configuration file.
+
     Args:
         config_name (str): The name of the configuration file.
+
     Returns:
         str: The path to the configuration file, or None if not found.
     """
@@ -58,6 +57,7 @@ def get_config_file_path(config_name):
 def load_config(config_name):
     """
     Load a specific configuration file.
+
     Args:
         config_name (str): The name of the configuration file.
     Returns:
@@ -70,7 +70,8 @@ def load_config(config_name):
 
 def load_service_mapping():
     """
-    Load the service mapping configuration.
+    Loads the service mapping configuration.
+
     Returns:
         dict: The service mapping configuration loaded from the JSON file.
     """
@@ -81,7 +82,15 @@ def load_service_mapping():
     return load_json(config_path)
 
 def get_service_config(service_name, key):
+    """
+    Get the configuration for a specific service and key (service functionality).
 
+    Args:
+        service_name (str): The name of the service.
+        key (str): The key representing the service functionality.
+    Returns:
+        dict: The loaded configuration data for the specified service and key, or None if not found
+    """
     service_mapping = load_service_mapping()
     if service_name in service_mapping:
         service_config = service_mapping[service_name]
